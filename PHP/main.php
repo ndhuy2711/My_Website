6af5718fa57591d96a411b675acb1f6fefa48513
NguyenDuongHuy <ndhuy2711@gmail.com>
2020-08-18 22:17:05 +0700

    Design Web

---
+++ PHP/main.php
@@ -0,0 +1,10 @@
+<?php 
+require ("../HTML/MVC_Client/head.html");
+require ("../HTML/MVC_Client/header.html");
+require ("../HTML/MVC_Client/nav.html");
+require ("../HTML/MVC_Client/banner.html");
+require ("../HTML/MVC_Client/mainLeft.html");
+require ("../HTML/MVC_Client/mainRight.html");
+require ("../HTML/MVC_Client/footer.html");
+
+?>
